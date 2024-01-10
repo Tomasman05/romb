98f21c33b732dd3faf024da21248bf3b79d5f836
Vito <lolozol80xd@gmail.com>
2024-01-10 10:39:04 +0100

    commit15

--- src/main/java/lan/zold/Romb.java
+++ src/main/java/lan/zold/Romb.java
@@ -1,8 +1,12 @@
 package lan.zold;
 
+import java.util.InputMismatchException;
+
 public class Romb {
     public double calcArea(double side, double angle){
-
+        if (side<=0) {
+            throw new InputMismatchException();
+        }
         return Math.pow(side, 2)*Math.sin(Math.toRadians(angle));
     }
 }
